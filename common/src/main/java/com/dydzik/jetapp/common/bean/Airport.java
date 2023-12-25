6f5b58d11fc173e2d66d5f535665fb0f85d83a72
Elovard <persall2013@gmail.com>
2023-12-25 19:35:13 +0300

    Add classes to common library

---
+++ common/src/main/java/com/dydzik/jetapp/common/bean/Airport.java
@@ -0,0 +1,31 @@
+package com.dydzik.jetapp.common.bean;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Airport {
+    private String name;
+    private List<String> boards = new ArrayList<>();
+    private int x;
+    private int y;
+
+    private void addBoard(String boardName) {
+        int ind = boards.indexOf(boardName);
+        if (ind >= 0) {
+            boards.set(ind, boardName);
+        } else {
+            boards.add(boardName);
+        }
+    }
+
+    private void removeBoard(String boardName) {
+        boards.remove(boardName);
+    }
+}
---
+++ common/src/main/java/com/dydzik/jetapp/common/bean/Source.java
@@ -0,0 +1,7 @@
+package com.dydzik.jetapp.common.bean;
+
+public enum Source {
+    BOARD,
+    OFFICE,
+    AIRPORT
+}
